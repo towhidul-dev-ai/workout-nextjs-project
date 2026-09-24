870ded5a53809c00c6f817c66dead06048d806f7
towhidulislamshahed25 <towhidulislamshahed1@gmail.com>
2026-09-24 21:49:26 +0600

    update cachine amd type

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,5 +1,5 @@
 export const getMuscle = async () =>{
-    const res = await fetch('https://api.abcz.workers.dev/api/fitlog', {cache: "force-cache"});
+    const res = await fetch('https://api.abcz.workers.dev/api/fitlog', {next: {revalidate: 15}});
     const data = await res.json();
     return data;
 }
