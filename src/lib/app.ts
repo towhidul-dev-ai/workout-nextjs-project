46e372aa0d0a1491c49e0ea05201982003259f30
towhidulislamshahed25 <towhidulislamshahed1@gmail.com>
2026-09-24 21:44:39 +0600

    added caching

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,5 +1,5 @@
 export const getMuscle = async () =>{
-    const res = await fetch('https://api.abcz.workers.dev/api/fitlog');
+    const res = await fetch('https://api.abcz.workers.dev/api/fitlog', {cache: "force-cache"});
     const data = await res.json();
     return data;
 }
